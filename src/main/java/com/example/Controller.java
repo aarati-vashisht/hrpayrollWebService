330a3e2522a16c7972c9b289192db8548181f7c0
aarati_vashisht <aarati.vashisht2804@gmail.com>
2019-03-29 12:00:20 +0530

    First Commit

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -14,33 +14,35 @@ public class Controller {
     HrPayRollService hrPayRollService;
 
     @PostMapping(value = "/Insert", produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String, String> Insert(@RequestBody HashMap<String, String> loginBody) throws NoSuchFieldException, IllegalAccessException {
+    public HashMap<String, String> Insert(@RequestBody HashMap<String, String> loginBody)  {
         return hrPayRollService.Insert(loginBody.get("QueryType"),
                 loginBody.get("StateName"),
                 loginBody.get("CreatedBy"),
                 loginBody.get("UpdatedBy"),
-                Integer.valueOf(loginBody.get ("CountryID")),
+                Integer.valueOf(loginBody.get("CountryID")),
                 Integer.valueOf(loginBody.get("StateID")));
 
     }
+
     @PostMapping(value = "/UPDATE", produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String, String> UPDATE(@RequestBody HashMap<String, String> loginBody) throws NoSuchFieldException, IllegalAccessException {
+    public HashMap<String, String> UPDATE(@RequestBody HashMap<String, String> loginBody) {
         return hrPayRollService.UPDATE(loginBody.get("QueryType"),
                 loginBody.get("StateName"),
                 loginBody.get("CreatedBy"),
                 loginBody.get("UpdatedBy"),
-                Integer.valueOf(loginBody.get ("CountryID")),
+                Integer.valueOf(loginBody.get("CountryID")),
                 Integer.valueOf(loginBody.get("StateID")));
+        //////new commit
 
     }
 
 
     @PostMapping(value = "/insertDataForCity", produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String, String> insertDataForCity(@RequestBody HashMap<String, String> loginBody) throws NoSuchFieldException, IllegalAccessException {
+    public HashMap<String, String> insertDataForCity(@RequestBody HashMap<String, String> loginBody)  {
         return hrPayRollService.insertDataForCity(loginBody.get("CityName"),
                 loginBody.get("CreatedBy"),
                 loginBody.get("UpdatedBy"),
-                Integer.valueOf(loginBody.get ("StateID")));
+                Integer.valueOf(loginBody.get("StateID")));
 
     }
 
